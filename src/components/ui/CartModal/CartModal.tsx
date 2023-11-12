8cea768d9ed872ec6e6a4edb746da9553356bb4e
unknown <luciano.giuliani92@gmail.com>
2023-11-12 12:30:08 -0300

    cambios en el style de hero y navbar

--- src/components/ui/CartModal/CartModal.tsx
+++ src/components/ui/CartModal/CartModal.tsx
@@ -4,12 +4,15 @@ import { FC } from "react";
 
 import { Table } from "../Table/Table";
 import { useNavigate } from "react-router-dom";
+import { useThemeContext } from "../../../context/ThemeContext";
 
 interface Props {
   handleShowCartModal: () => void;
 }
 
 export const CartModal: FC<Props> = ({ handleShowCartModal }) => {
+
+ 
   const navigate = useNavigate();
 
   const handleNavigate = () => {
@@ -18,7 +21,7 @@ export const CartModal: FC<Props> = ({ handleShowCartModal }) => {
   };
 
   return (
-    <div className={styles.modalContainer}>
+    <div className={`${styles.modalContainer}`}>
       <button className={styles.modalCloseButton} onClick={handleShowCartModal}>
         <img src={Close} alt="close" />
       </button>
